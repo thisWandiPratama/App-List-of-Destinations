4e5ced67faf36f09105c7cac3ca050ca29a6f796
OneBlog <wandi99pratama@gmail.com>
2019-10-24 14:33:28 +0700

    Upload Gambar Dokumentasi

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -24,20 +24,23 @@ class AddTodo extends React.Component{
 
   render(){
     return(
-        <View style={{flexDirection:'row', marginHorizontal: 20}}>
-            <TextInput
-              onChangeText= { (text) => this.setState({text}) }
-              value = { this.state.text}
-              placeholder ='Add Your Target'
-              style = {{ borderWidth: 1, borderColor:'#f2f2e1',backgroundColor:'#eaeaea',
-              height: 50, flex:1, padding: 5}}
-            />
+      <View>
+        <View style = {{alignItems:'center', marginBottom:10}}><Text style = {{fontSize:20}}> List of Destinations </Text></View>
+          <View style={{flexDirection:'row', marginHorizontal: 20}}>
+              <TextInput
+                onChangeText= { (text) => this.setState({text}) }
+                value = { this.state.text}
+                placeholder ='Add Your Target'
+                style = {{ borderWidth: 1, borderColor:'#f2f2e1',backgroundColor:'#eaeaea',
+                height: 50, flex:1, padding: 5}}
+              />
 
-            <TouchableOpacity onPress= { () => this.addTodo(this.state.text) }>
-                <View style={{height: 50, backgroundColor:'#eaeaea', alignItems:'center', justifyContent:'center'}}>
-                    <Text>Add</Text>
-                </View>
-            </TouchableOpacity>
+              <TouchableOpacity onPress= { () => this.addTodo(this.state.text) }>
+                  <View style={{height: 50, backgroundColor:'#eaeaea', alignItems:'center', justifyContent:'center'}}>
+                      <Text>Add</Text>
+                  </View>
+              </TouchableOpacity>
+          </View>
         </View>
       )
     }
